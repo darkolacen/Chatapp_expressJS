8f154928206187a144b9b840fe8159038e7e7513
Unknown <pander11@mail.ccsf.edu>
2018-01-16 17:33:15 +0100

    ni vec /newmsg

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,21 @@
 $(function () {
     var socket = io();
     $('form').submit(function(){
-      socket.emit('chat message', $('#m').val());
-      
-      return true;
+
+    var now = new Date();
+    // Cas in datum v JSON format
+    var jsonDate = now.toJSON();
+      var msg ={
+      	user_g_id: $('#g_id').val(),
+      	name: $('#name').val(),
+      	msg_content: $('#m').val(),
+      	date_time: jsonDate
+      }
+      socket.emit('chat message', msg);
+      $('#m').val('');
+      return false;
     });
     socket.on('chat message', function(msg){
-      $('#messages').append($('<li>').text(msg));
+      $('#messages').append($('<li>').text(msg.name + " : " + msg.msg_content));
     });
   });
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ var url = "mongodb://localhost:27017/mydb";
 
 
 
+
 function isUserAuthenticated(req, res, next) {
 
   if (req.session.user) {
@@ -41,33 +42,33 @@ router.get('/', isUserAuthenticated, function(req, res, next) {
   
 });
 
-router.post('/newmsg', function(req, res, next) {
-  var now = new Date();
-  // Cas in datum v JSON format
-  var jsonDate = now.toJSON();
-  //Cas in datum iz JSON v JS
-  //var normalDate = new Date(jsonDate);
-  var msg = {
-    user_g_id: req.session.user.g_id,
-    name: req.session.user.name,
-    msg_content: req.body.sporocilo,
-    date_time: jsonDate
-  };
+// router.post('/newmsg', function(req, res, next) {
+//   var now = new Date();
+//   // Cas in datum v JSON format
+//   var jsonDate = now.toJSON();
+//   //Cas in datum iz JSON v JS
+//   //var normalDate = new Date(jsonDate);
+//   var msg = {
+//     user_g_id: req.session.user.g_id,
+//     name: req.session.user.name,
+//     msg_content: req.body.sporocilo,
+//     date_time: jsonDate
+//   };
 
-  MongoClient.connect(url, function(err, client) {
-      var db = client.db('mydb');
-      if (err) throw err;
+//   MongoClient.connect(url, function(err, client) {
+//       var db = client.db('mydb');
+//       if (err) throw err;
       
-      db.collection("Messages").insertOne(msg, function(err, res) {
-        if (err) throw err;
-        console.log("1 msg inserted");
-        client.close();
-      });
+//       db.collection("Messages").insertOne(msg, function(err, res) {
+//         if (err) throw err;
+//         console.log("1 msg inserted");
+//         client.close();
+//       });
       
-});
+// });
 
-  res.redirect('/');
-});
+//   res.redirect('/');
+// });
 
 
 module.exports = router;
