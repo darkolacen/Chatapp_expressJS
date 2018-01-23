47c94a03f79d286db17f6ea52be4ec633fd5bcbe
Unknown <pander11@mail.ccsf.edu>
2018-01-23 18:16:24 +0100

    file upload

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -6,6 +6,11 @@ var http = require('http').Server(express),
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 var ObjectId = require('mongodb').ObjectId;
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
+
+
+
 
 function isUserAuthenticated(req, res, next) {
 
@@ -21,14 +26,20 @@ router.post('/', isUserAuthenticated, function(req, res, next) {
   var chatRoomID = req.body.chatRoomID;
   var chatRoomName = req.body.chatRoomName;
   var allUsers = null;
+  var allFiles = null;
   MongoClient.connect(url, function(err, client) {
     var db = client.db('mydb');
     if (err) throw err;
 
     db.collection("ChatRooms").findOne({_id: ObjectId(chatRoomID)}, (err, result) => {
-      console.log(result.users);
       allUsers = result.users;
-      console.log(allUsers);
+      client.close();
+    });
+
+    db.collection("Files").find({"chatRoomID" : chatRoomID}).toArray(function(err, result) {
+      if (err) throw err;
+      allFiles = result;
+
       client.close();
     });
       
@@ -41,7 +52,8 @@ router.post('/', isUserAuthenticated, function(req, res, next) {
         allMessages: result,
         chatRoomID: chatRoomID,
         chatRoomName: chatRoomName,
-        allUsers: allUsers
+        allUsers: allUsers,
+        allFiles: allFiles
       });
 
       client.close();
@@ -49,6 +61,12 @@ router.post('/', isUserAuthenticated, function(req, res, next) {
     
   });
   
+});
+
+router.get('/download/fileName/:fileName/originalName/:originalName',isUserAuthenticated,(req, res) => {
+  var file = req.params;
+
+  res.download('./uploads/' + file.fileName, file.originalName); 
 });
 
 
@@ -69,6 +87,31 @@ router.post('/addUser', isUserAuthenticated, function(req, res, next) {
   });
   res.redirect('/');
   
+});
+
+router.post('/upload', upload.single('dat'), (req, res) => {
+
+  var fileToUpload = {
+    chatRoomID: req.body.chatRoomID,
+    originalName: req.file.originalname,
+    fileName: req.file.filename
+  }
+
+
+  MongoClient.connect(url, function(err, client) {
+    var db = client.db('mydb');
+    if (err) throw err;
+    
+    db.collection("Files").insertOne(fileToUpload, function(err, res) {
+      if (err) throw err;
+      console.log("1 File inserted");
+      client.close();
+    });
+
+  });
+
+  console.log(req.file);
+  res.redirect('/');
 });
 
 
